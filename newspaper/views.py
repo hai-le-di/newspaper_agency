0fb0699c183ff6bdd3300bdb3d1497d639333b30
Diana <daria.blessed18@gmail.com>
2023-01-27 22:10:42 +0200

    Create index view in views.py

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import (Redactor,
+                              Newspaper,
+                              Topic)
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
