352ebfcfd458a725b0fb59d05f77998c2c548031
Diana <daria.blessed18@gmail.com>
2023-01-28 00:20:06 +0200

    Add NewspaperSearchForm & TopicSearchForm & RedactorSearchForm to forms.py

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -44,3 +44,30 @@ class NewspaperForm(forms.ModelForm):
     class Meta:
         model = Newspaper
         fields = "__all__"
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=66,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by title"})
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=66,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by topic"})
+    )
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username.."})
+    )
