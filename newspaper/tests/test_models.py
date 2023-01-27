fded95d01b60641fbfab5d47c121d4ec87fb6d7e
Diana <daria.blessed18@gmail.com>
2023-01-28 00:37:00 +0200

    Create tests/test_models.py

---
+++ newspaper/tests/test_models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from newspaper.models import Redactor, Newspaper, Topic
+
+
+class ModelsTests(TestCase):
+
+    def test_topic_str(self):
+        topic = Topic.objects.create(name="test")
+        self.assertEqual(str(topic), "test")
+
+    def test_redactor_str(self):
+        redactor = Redactor.objects.create(username="test",
+                                           password="12345",
+                                           first_name="test first",
+                                           last_name="test last",
+                                           years_of_experience=3)
+        self.assertEqual(str(redactor), "test (test first test last)")
+
+    def test_newspaper_str(self):
+        published_date = datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
+        topic = Topic.objects.create(name="test")
+        redactor = Redactor.objects.create(username="test",
+                                           password="12345",
+                                           first_name="test first",
+                                           last_name="test last",
+                                           years_of_experience=3)
+        newspaper = Newspaper.objects.create(title="test",
+                                             content="test content",
+                                             published_date=published_date,
+                                             topic=topic,
+                                             )
+        self.assertEqual(
+            str(newspaper), f"{newspaper.title} ({newspaper.topic},"
+                            f" {newspaper.published_date})"
+        )
