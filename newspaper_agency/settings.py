94db52a1e325f706a0b654e3d9a7716a1eaf10c1
Diana <daria.blessed18@gmail.com>
2023-02-03 23:37:21 +0200

    Delete redundant static files

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -28,8 +28,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'newspaper-agency-jx6a.onrender.com',
-]
+    'newspaper-agency-jx6a.onrender.com',]
 
 
 # Application definition
